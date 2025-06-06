c6071b3e2cb541dad57205ff3001d220812659d0
Deep-170 <singhadeep2005@gmail.com>
2025-06-06 13:33:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// You can add interactivity later
+console.log("Welcome to Deep's Portfolio!");
+
+// Example: Alert on page load
+window.onload = function() {
+  console.log("Page loaded successfully.");
+};
